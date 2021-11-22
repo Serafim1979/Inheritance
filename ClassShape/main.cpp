ceea07ae72f70c1aea68e14d2fdabe522cfbc6bc
Ivan Korolkov <korolkov.ivan@mail.ru>
2021-11-23 02:57:02 +0300

    Created base structure

--- ClassShape/main.cpp
+++ ClassShape/main.cpp
@@ -1,4 +1,40 @@
 #include<iostream>
+#include<string>
+
+class Shape
+{
+private:
+    std::string name;
+
+public:
+    Shape(std::string name)
+    {
+        this->name = name;
+    }
+};
+
+class Circle : public Shape
+{
+private:
+    std::string name;
+
+public:
+
+};
+
+class Square : public Shape
+{
+private:
+    std::string name;
+public:
+};
+
+class Triangle : public Shape
+{
+private:
+public:
+
+};
 
 int main()
 {
