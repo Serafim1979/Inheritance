36ec4981e9f347f01df90c7952ce02f45a6ed4be
Ivan Korolkov <korolkov.ivan@mail.ru>
2021-11-24 22:37:09 +0300

    Made minor changes to the original structure

--- ClassShape/main.cpp
+++ ClassShape/main.cpp
@@ -4,13 +4,32 @@
 class Shape
 {
 private:
-    std::string name;
+    double x, y;
 
 public:
-    Shape(std::string name)
+    Shape(double x = 0., double y = 0.)
     {
-        this->name = name;
+        this->x = x > 0 ? x : 0.;
+        this->y = y > 0 ? y : 0.;
     }
+    virtual ~Shape(){}
+    virtual double area()const = 0;}
+    virtual double perimeter()const = 0;}
+};
+
+class Rect : public Shape
+{
+private:
+    double height, width;
+public:
+    Rect(double height, double width)
+    {
+        this->height = height > 0 ? height : 0.;
+        this->width = width > 0 ? width : 0.;
+    }
+    ~Rect(){}
+    double area()const{return height * width;}
+    double perimeter()const{return 2 * (height + width);}
 };
 
 class Circle : public Shape
@@ -38,6 +57,9 @@ public:
 
 int main()
 {
+    Shape sh(2., 3.5);
+    Rect rct(3, 4);
+    std::cout << rct.area() << " " << rct.perimeter() << std::endl;
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
